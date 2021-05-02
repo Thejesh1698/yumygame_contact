a88ab53b55fe427b54264e8ecff70dd5b83ec6a4
Thejesh <thejesh1698@gmail.com>
2021-05-02 14:21:34 +0530

    basic spring-boot setup

---
+++ spring-boot-assignment/task/src/test/java/com/product/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
